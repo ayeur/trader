6a4b43f03f171ac19b4eca32e2842d836b9163d3
ayeur <asher.reg@gmail.com>
2017-12-03 01:02:00 -0500

    Scrapes bitcoin json data. Powered by Coinbase.

--- window.py
+++ window.py
@@ -1,26 +1,14 @@
-from tkinter import *
 from requests import *
+import json
+import time
+import os
 
-r = get('https://api.coindesk.com/v1/bpi/currentprice.json', params=None)
+save_path = os.getcwd() + '/data'
+int = 0
 
-def center(toplevel):
-    toplevel.update_idletasks()
-    w = toplevel.winfo_screenwidth()
-    h = toplevel.winfo_screenheight()
-    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
-    x = w/2 - size[0]/2
-    y = h/2 - size[1]/2
-    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
-
-
-window = Tk()
-window.title("Trader")
-window.minsize(50,50)
-T = Text(window, height=50, width=50)
-T.pack()
-T.insert(END, r.text)
-center(window)
-mainloop()
-
-
-window.mainloop()
+while True:
+    int += 1
+    bit = get('https://api.coindesk.com/v1/bpi/currentprice.json', params=None)
+    with open(os.path.join(save_path,'data' +str (int) + '.txt'), "w") as outfile:
+        json.dump(bit.text, outfile)
+        time.sleep(5)
